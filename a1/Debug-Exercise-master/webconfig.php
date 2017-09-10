f2c9cd8d218d3bf2a7805e8264cab201ed8e9868
Etschrock <etschrock@yahoo.com>
2017-09-10 18:50:14 -0400

    Exercise 1, php debugging

---
+++ a1/Debug-Exercise-master/webconfig.php
@@ -0,0 +1,4 @@
+<?php
+
+define('ABSOLUTE_PATH', '/home/etschroc/htdocs/CIT313/FA2017/a1/Debug-Exercise-master');
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:21971/CIT313/FA2017/a1/Debug-Exercise-master');
