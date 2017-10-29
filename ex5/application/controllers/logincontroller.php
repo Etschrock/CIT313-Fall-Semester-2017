77155172bcdb3f207108658c20aef77f9ed5562d
Etschrock <etschrock@yahoo.com>
2017-10-29 12:54:09 -0400

    example 5. logout and protected pages

---
+++ ex5/application/controllers/logincontroller.php
@@ -0,0 +1,55 @@
+<?php
+
+class LoginController extends Controller{
+	
+	protected $userObject;
+
+    public function defaultTask(){
+
+        $this->userObject = new User();
+        $this->set('task', 'do_login');
+
+    }
+
+   public function do_login() {
+
+       $this->userObject = new User();
+
+       if ($this->userObject->checkUser($_POST['email'],$_POST['password'])) {
+
+           $userInfo = $this->userObject->getUserFromEmail($_POST['email']);
+
+           $_SESSION['uID'] = $userInfo['uID'];
+
+           if(strlen($_SESSION['redirect']) > 0) {
+               $view = $_SESSION['redirect'];
+               unset($_SESSION['redirect']);
+
+               header ('Location:' . BASE_URL . $view);
+           } else {
+
+               header('Location: ' . BASE_URL);
+
+           }
+
+       } else {
+
+           $this->set('error', 'Wrong password or Email combination.');
+       }
+
+   }
+
+   public function logout() {
+
+       //unset session id
+       unset($_SESSION['uID']);
+
+       //close the session
+       session_write_close();
+
+       //send them back to the home page
+       header('Location: ' . BASE_URL);
+   }
+	
+	
+}
